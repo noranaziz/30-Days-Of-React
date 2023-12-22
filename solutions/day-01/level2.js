e92656015eb73f160410ca591c2f9ca32cae4247
noranaziz <noranaziz93@gmail.com>
2023-12-22 16:53:02 -0500

    day 1 - javascript refresher

---
+++ solutions/day-01/level2.js
@@ -0,0 +1,32 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+];
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+];
+
+// change string to array
+let text = "I love teaching and empowering people I teach HTML CSS JS React Python";
+const words = text.split(" ");
+console.log(words);
+console.log(words.length);
+
+// add, remove, edit items
+const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
