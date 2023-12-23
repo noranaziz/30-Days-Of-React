71cd8982f461a3b9d336f8e6921d5fdffd29a39e
noranaziz <noranaziz93@gmail.com>
2023-12-23 17:42:01 -0500

    day 1 - javascript refresher

--- solutions/day-01/level2.js
+++ solutions/day-01/level2.js
@@ -12,7 +12,7 @@ const countries = [
     'Kenya',
 ];
   
-  const webTechs = [
+const webTechs = [
     'HTML',
     'CSS',
     'JavaScript',
@@ -30,3 +30,29 @@ console.log(words.length);
 
 // add, remove, edit items
 const shoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+shoppingCart.unshift("Meat");
+shoppingCart.push("Sugar");
+const teaIndex = shoppingCart.indexOf("Tea");
+shoppingCart[teaIndex] = "Green Tea";
+
+// check if ethiopia exists in countries array. if it exists, print ETHIOPIA, if it doesn't add it to countries
+if(countries.includes("Ethiopia")){
+  const ethIndex = countries.indexOf("Ethiopia");
+  countries[ethIndex] = countries[ethIndex].toUpperCase();
+} else {
+  countries.push("Ethiopia");
+}
+
+// check if Sass exists in webTechs array. if it exists print "Sass is a CSS preprocess", if it doesn't exist add Sass to array and print array
+if(webTechs.includes("Sass")){
+  console.log("Sass is a CSS preprocess");
+} else {
+  webTechs.push("Sass");
+  console.log(webTechs);
+}
+
+// concat the two variables and store it in a fullStack var
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node', 'Express', 'MongoDB'];
+const fullStack = frontEnd.concat(backEnd);
+console.log(fullStack);
