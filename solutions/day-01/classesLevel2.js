d9eb846eb63f4aba51552d5f1d6f9a991f9b24cf
noranaziz <noranaziz18@icloud.com>
2023-12-25 15:40:13 -0500

    day 1 - javascript refresher

--- solutions/day-01/classesLevel2.js
+++ solutions/day-01/classesLevel2.js
@@ -16,3 +16,31 @@ class Animal {
 }
 const animal = new Animal("steve", 5, "blue", 4);
 console.log(animal);
+
+class Dog extends Animal {
+    constructor(name, age, color, legs, breed){
+        super(name, age, color, legs);
+        this.breed = breed;
+    }
+    // override
+    eat(){
+        console.log("the dog eats kibble.");
+    }
+}
+const dog = new Dog("spot", 6, "black", 4, "lab");
+console.log(dog);
+dog.eat();
+
+class Cat extends Animal {
+    constructor(name, age, color, legs, affectionate){
+        super(name, age, color, legs);
+        this.affectionate = affectionate;
+    }
+    // override
+    drink(){
+        console.log("the cat drinks milk.");
+    }
+}
+const cat = new Cat("shadow", 2, "white", 4, true);
+console.log(cat);
+cat.drink();
