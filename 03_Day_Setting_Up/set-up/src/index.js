da635dbec6d7ba4d283eec50a2cd3bcf4fec3f3d
noranaziz <noranaziz18@icloud.com>
2024-01-03 11:10:33 -0500

    day 3 - set up react app

--- 03_Day_Setting_Up/set-up/src/index.js
+++ 03_Day_Setting_Up/set-up/src/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-const jsxElement = <h1>this is a jsx element.</h1>
+// header
+const header = (
+  <header>
+    <h1>welcome to 30 days of react</h1>
+    <h2>setting up react</h2>
+    <h3>js library</h3>
+    <p>Noran Abdel-Aziz</p>
+    <small>December 27th, 2023</small>
+  </header>
+)
+
+// main
+const main = (
+  <main>
+    <p>prereq to get started react.js:</p>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
+  </main>
+)
+
+// footer
+const footer = (
+  <footer>
+    <p>Copyright 2023</p>
+  </footer>
+)
+
+// header, main, and footer
+const app = (
+  <div>
+    {header}
+    {main}
+    {footer}
+  </div>
+)
+
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(jsxElement, rootElement)
+ReactDOM.render(app, rootElement)
