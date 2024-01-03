cfbb58d653b7d36fcae56d1a3975e3618cd289a5
noranaziz <noranaziz18@icloud.com>
2024-01-03 13:12:12 -0500

    injected data to jsx elements

--- 03_Day_Setting_Up/set-up/src/index.js
+++ 03_Day_Setting_Up/set-up/src/index.js
@@ -11,16 +11,41 @@ const headerStyles = {
 }
 */
 
+const welcome = 'welcome to 30 days of react'
+const title = 'setting up react'
+const subtitle = 'js library'
+const author = {
+  firstName: 'Noran',
+  lastName: 'Abdel-Aziz',
+}
+const date = 'January 3rd, 2024'
+
 const header = (
   <header>
     <div className = 'header-wrapper'>
-      <h1>welcome to 30 days of react</h1>
-      <h2>setting up react</h2>
-      <h3>js library</h3>
-      <p>Noran Abdel-Aziz</p>
-      <small>December 27th, 2023</small>
+      <h1>{welcome}</h1>
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>{author.firstName} {author.lastName}</p>
+      <small>{date}</small>
     </div>
   </header>
+)
+
+const numOne = 3
+const numTwo = 2
+const result = (
+  <p>{numOne} + {numTwo} = {numOne + numTwo}</p>
+)
+
+const yearBorn = 2003
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+  <p>
+    {' '}
+    {author.firstName} {author.lastName} is {age} years old
+  </p>
 )
 
 // main
@@ -29,6 +54,9 @@ const mainStyles = {
   backgroundColor: '#F3F0F5'
 }
 */
+
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
 
 const main = (
   <main>
@@ -39,11 +67,9 @@ const main = (
         </strong>
         :
       </p>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-      </ul>
+      <ul>{techsFormatted}</ul>
+      {result}
+      {personAge}
     </div>
   </main>
 )
@@ -55,10 +81,12 @@ const footerStyles = {
 }
 */
 
+const copyright = 'Copyright 2023'
+
 const footer = (
   <footer>
     <div className='footer-wrapper'>
-      <p>Copyright 2023</p>
+      <p>{copyright}</p>
     </div>
   </footer>
 )
