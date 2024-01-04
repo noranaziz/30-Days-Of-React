b7e5d607f9098e47efd6d9a0c922d38ae4af0f6d
noranaziz <noranaziz18@icloud.com>
2024-01-04 13:21:49 -0500

    set up exercise & added css

--- solutions/day-03/day-3-exercise/src/index.js
+++ solutions/day-03/day-3-exercise/src/index.js
@@ -2,6 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const helloWorld = <h1>hello world</h1>
+const title = (
+  <div className='title-wrapper'>
+    <h1>subscribe</h1>
+  </div>
+)
+const subtitle = (
+  <div className='subtitle-wrapper'>
+    <p>sign up with your email address to receive news and updates.</p>
+  </div>
+)
+
+const firstNameInput = <input id='input1' type='text' placeholder='first name'></input>
+const lastNameInput = <input id='input2' type='text' placeholder='last name'></input>
+const emailInput = <input id='input3' type='text' placeholder='email'></input>
+const inputs = (
+  <div className='inputs-wrapper'>
+    {firstNameInput} {lastNameInput} {emailInput}
+  </div>
+)
+const subscribeBtn = <button></button>
+
+const app = (
+  <div className='app'>
+    {title}
+    {subtitle}
+    {inputs}
+    {subscribeBtn}
+  </div>
+)
+
 const rootElement = document.getElementById('root')
-ReactDOM.render(helloWorld, rootElement)
+ReactDOM.render(app, rootElement)
