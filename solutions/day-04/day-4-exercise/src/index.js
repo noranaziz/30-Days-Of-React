3e070bd6566cd437c3c247cf6f06c5b03d10e041
noranaziz <noranaziz18@icloud.com>
2024-01-06 14:54:05 -0500

    adjusted padding (moved colors closer together) and rounded corners

--- solutions/day-04/day-4-exercise/src/index.js
+++ solutions/day-04/day-4-exercise/src/index.js
@@ -22,6 +22,7 @@ const HexaColor = () => {
     color: 'white',
     height: '50px',
     padding: '10px',
+    borderRadius: '5px'
   }
 
   return <div style={colorStyle}>{color}</div>
@@ -29,19 +30,19 @@ const HexaColor = () => {
 
 const App = () => (
   <div className='app'>
-    <div>
+    <div className='color-1'>
       <HexaColor/>
     </div>
-    <div>
+    <div className='color-2'>
       <HexaColor/>
     </div>
-    <div>
+    <div className='color-3'>
       <HexaColor/>
     </div>
-    <div>
+    <div className='color-4'>
       <HexaColor/>
     </div>
-    <div>
+    <div className='color-5'>
       <HexaColor/>
     </div>
   </div>
